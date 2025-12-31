2fbb0be6b1996074585eecd5c169e84354e430f8
Kim Văn Toản <kimvantoan2k3@gmail.com>
2025-12-31 15:21:05 +0700

    git commit -m "init project"

---
+++ src/main/java/com/smart_finance/smart_finance_be/SmartFinanceBeApplication.java
@@ -0,0 +1,13 @@
+package com.smart_finance.smart_finance_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartFinanceBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartFinanceBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/smart_finance/smart_finance_be/SmartFinanceBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smart_finance.smart_finance_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartFinanceBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
