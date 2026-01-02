4e6cc1b698dc5b219c871ce25a073b4ea65b4d3a
Kim Văn Toản <kimvantoan2k3@gmail.com>
2026-01-02 14:54:57 +0700

    chore: create base entity

---
+++ src/main/java/com/smart_finance/smart_finance_be/cmmn/base/BaseEntity.java
@@ -0,0 +1,38 @@
+package com.smart_finance.smart_finance_be.cmmn.base;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.PrePersist;
+import jakarta.persistence.PreUpdate;
+import lombok.Data;
+
+@MappedSuperclass
+@Data
+public abstract class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "created_at", updatable = false)
+    private LocalDateTime createdAt;
+
+    @Column(name = "updated_at")
+    private LocalDateTime updatedAt;
+
+    @PrePersist
+    protected void onCreate() {
+        createdAt = LocalDateTime.now();
+        updatedAt = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        updatedAt = LocalDateTime.now();
+    }
+}
