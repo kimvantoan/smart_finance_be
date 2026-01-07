698afedaebef4e441a69e927bbbf14a02439b70d
Kim Văn Toản <kimvantoan2k3@gmail.com>
2026-01-07 14:37:18 +0700

    chore: update response for transaction

---
+++ src/main/java/com/smart_finance/smart_finance_be/payload/response/TransactionProjection.java
@@ -0,0 +1,16 @@
+package com.smart_finance.smart_finance_be.payload.response;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+import com.smart_finance.smart_finance_be.entity.CategoryType;
+
+public interface TransactionProjection {
+    Long getId();
+    Long getCategoryId();
+    CategoryType getType();
+    BigDecimal getAmount();
+    LocalDate getTransactionDate();
+    String getNote();
+
+}
--- src/main/java/com/smart_finance/smart_finance_be/repository/TransactionRepository.java
+++ src/main/java/com/smart_finance/smart_finance_be/repository/TransactionRepository.java
@@ -9,12 +9,13 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.smart_finance.smart_finance_be.entity.Transaction;
+import com.smart_finance.smart_finance_be.payload.response.TransactionProjection;
 
 @Repository
 public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 
     @Query(value = """
-            SELECT *
+            SELECT t.id, t.category_id, t.type, t.amount, t.transaction_date, t.note
             FROM transactions t
             WHERE t.user_id = :userId
                 AND t.deleted = false
@@ -24,7 +25,7 @@ public interface TransactionRepository extends JpaRepository<Transaction, Long> 
                 AND (:year IS NULL OR YEAR(t.transaction_date) = :year)
             ORDER BY t.transaction_date DESC
                 """, nativeQuery = true)
-    List<Transaction> findByUserId(
+    List<TransactionProjection> findByUserId(
         @Param("userId") Long userId, 
         @Param("type") String type, 
         @Param("categoryId") Long categoryId, 
